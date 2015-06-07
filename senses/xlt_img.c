ee93a8aa49be628fbd215b9d379946d5ee2b4464
bjornstahl <arcan-fe@nospam>
2015-06-07 12:12:04 +0200

    activated xlt- img, not working yet

--- senses/xlt_img.c
+++ senses/xlt_img.c
@@ -15,11 +15,10 @@
 #include "font_8x8.h"
 
 enum view_mode {
-	VIEW_AUTO = 0,
-	VIEW_AUTO_SIMPLE,
-	VIEW_MANUAL,
-	VIEW_DECODE,
-	VIEW_DECODED
+	VIEW_LIST = 0,
+	VIEW_AUTO = 1,
+	VIEW_MANUAL = 2,
+	VIEW_LAST = 3
 };
 
 /*
@@ -38,27 +37,44 @@ struct xlti_ctx {
 	enum view_mode current, last;
 };
 
-/*
- * GIF: (87a) 47 49 46 38 37 61
-        (89a) 47 49 46 38 39 61
- *
- * PNG: 89 50 4E 47 0D 0A 1A 0A
- * JPEG: FF D8 FF E0
- * BMP: 42 4D
- */
+struct {
+	char ident[8];
+	uint8_t buf[10];
+	size_t used;
+} magic[] = {
+	{
+		.ident = "GIF87",
+		.buf = {0x47, 0x49, 0x46, 0x38, 0x37, 0x61},
+		.used = 6
+	},
+	{
+		.ident = "GIF89",
+		.buf = {0x47, 0x49, 0x46, 0x38, 0x39, 0x61},
+		.used = 6
+	},
+	{
+		.ident = "PNG",
+		.buf = {0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a},
+		.used = 8
+	},
+	{
+		.ident = "BMP",
+		.buf = {0x42, 0x4d},
+		.used = 2
+	}
+};
 
 static bool input(struct arcan_shmif_cont* out, arcan_event* ev)
 {
+
 	struct xlti_ctx* ctx = out->user;
 	if (strcmp(ev->io.label, "TAB") == 0){
-		if (ctx->current >= VIEW_DECODE)
-			ctx->current = ctx->last;
-		else
-			ctx->current = (ctx->current + 1) % VIEW_DECODE;
+		ctx->current = (ctx->current + 1) % VIEW_LAST;
+		return true;
 	}
 	else if (strcmp(ev->io.label, "ENTER") == 0 && ctx->found != -1){
 		ctx->last = ctx->current;
-		ctx->current = VIEW_DECODE;
+		ctx->current = VIEW_MANUAL;
 	}
 	else
 		return false;
@@ -66,53 +82,64 @@ static bool input(struct arcan_shmif_cont* out, arcan_event* ev)
 	return true;
 }
 
+/*
+ * Return a list of possible starting offset and magic[] indexes for the hairy
+ * case of supporting decoding and detection across sample window edges, we'll
+ * need some buffer support class that is invalidated across seeks. Something
+ * for later.
+ */
+struct scanres {
+	uint8_t magic;
+	size_t ofs;
+};
+static size_t scan(size_t buf_sz, uint8_t* buf, struct scanres** out)
+{
+	size_t magic_count = sizeof(magic) / sizeof(magic[0]);
+	size_t oc[sizeof(magic)/sizeof(magic[0])] = {0};
+
+	for (size_t i = 0; i < buf_sz; i++)
+		for (size_t j = 0; j < magic_count; j++)
+			if (buf[i] == magic[j].buf[ oc[j] ]){
+				oc[j]++;
+				if (oc[j] >= magic[j].used){
+					printf("Match: %s\n", magic[j].ident);
+					oc[j] = 0;
+				}
+			}
+			else
+				oc[j] = 0;
+
+	*out = NULL;
+	return 0;
+}
+
 static bool populate(bool newdata, struct arcan_shmif_cont* in,
 	struct arcan_shmif_cont* out, uint64_t pos, size_t buf_sz, uint8_t* buf)
 {
-	struct xlti_ctx* ctx = out->user;
-
 	if (!buf){
 		free(out->user);
 		out->user = NULL;
 		return false;
 	}
 
+/* we work with the entire buffer at once, so only scan when
+ * something has changed */
+	if (!newdata)
+		return false;
+
+	struct xlti_ctx* ctx = out->user;
 	if (!out->user){
-		out->user = malloc(sizeof(struct xlti_ctx));
-		memset(out->user, sizeof(struct xlti_ctx), '\0');
+		ctx = out->user = malloc(sizeof(struct xlti_ctx));
+		memset(ctx, sizeof(struct xlti_ctx), '\0');
 		ctx->current = VIEW_AUTO;
 		ctx->found = -1;
 	}
 
-/*
-	size_t ofs = 0;
-	int w, h, c;
- */
+/* depending on view mode, we autodecode first or just list matches */
+	struct scanres* res;
+	scan(buf_sz, buf, &res);
 
-/* [ scan mode ]
- * 1. sweep magic values from table in comments,
- *    check matches against stbi_info, if reasonable: add to scanlist
- *    (ofs, type)
- */
-
-/* [ basic decode ]
-	uint32_t* imgbuf = (uint32_t*) stbi_load_from_memory(
-		(stbi_uc const*) buf + ctx->found, buf_sz - ctx->found, &w, &h, &c, 4);
-
-	if (-1 == ctx->found){
-		draw_box(out, 0, 0, out->addr->w, out->addr->h,
-			RGBA(0x00, 0x00, 0x00, 0xff));
-	}
-	else {
-		char tmpbuf[ sizeof("Possible Match: XXX, 9999x9999x4") ];
-		snprintf(tmpbuf, sizeof(tmpbuf), "Possible Match: %.3s, %.4d*%.f4dx%.1d",
-			stbi_lut[type], w, h, c);
-
-		draw_text(out, tmpbuf, 4, 4, RGBA(0x00, 0xff, 0x00, 0xff));
-	}
-*/
-
-	return true;
+	return false;
 }
 
 int main(int argc, char** argv)
